8ab852ed90bfe4fa9e7ef42c9bf0982c086a2ef6
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-05-03 22:28:00 +0530

    chore: added wallpaper and fixed helpers

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,12 +1,20 @@
-import { Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView, Image, View } from "react-native";
 import React from "react";
 import "../global.css";
+import { StatusBar } from "expo-status-bar";
+import images from "../constants/images";
+import { heightPercentage, widthPercentage } from "../helpers/common";
 
 const Welcome = () => {
   return (
-    <SafeAreaView className="bg-grayBG">
-      <Text className="text-2xl font-black">Welcome</Text>
-    </SafeAreaView>
+    <View className="flex">
+      <StatusBar style="light" />
+      <Image
+        source={images.mobile}
+        className={`${widthPercentage(100)} ${heightPercentage(100)} `}
+        resizeMode="cover"
+      />
+    </View>
   );
 };
 
--- src/helpers/common.js
+++ src/helpers/common.js
@@ -3,11 +3,11 @@ import { Dimensions } from "react-native";
 const { width, height } = Dimensions.get("window");
 
 export const widthPercentage = (widthPercent) => {
-  const elemWidth = parseFloat(widthPercent);
-  return PixelRatio.roundToNearestPixel((width * elemWidth) / 100);
+  const percentageWidth = Math.round((width * widthPercent) / 100);
+  return `w-[${percentageWidth}px]`;
 };
 
 export const heightPercentage = (heightPercent) => {
-  const elemHeight = parseFloat(heightPercent);
-  return PixelRatio.roundToNearestPixel((height * elemHeight) / 100);
+  const percentageHeight = Math.round((height * heightPercent) / 100);
+  return `h-[${percentageHeight}px]`;
 };
