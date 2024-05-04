1582afd7890a1d014ea4b2e07224dc95ffcda6af
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-05-04 12:11:29 +0530

    chore: added ts

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -2,12 +2,12 @@ import { Dimensions } from "react-native";
 
 const { width, height } = Dimensions.get("window");
 
-export const widthPercentage = (widthPercent) => {
+export const widthPercentage = (widthPercent: number) => {
   const percentageWidth = Math.round((width * widthPercent) / 100);
   return percentageWidth;
 };
 
-export const heightPercentage = (heightPercent) => {
+export const heightPercentage = (heightPercent: number) => {
   const percentageHeight = Math.round((height * heightPercent) / 100);
   return percentageHeight;
 };
